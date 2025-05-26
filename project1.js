dd404726284000acc551b2b9b57e6a32c036d347
Yadnesh <yadneshneel18@gmail.com>
2025-05-26 22:49:38 +0530

    First Project done

---
+++ project1.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (button) {
+    button.addEventListener('click',function(e){
+         console.log(e)
+         console.log(e.target)
+         switch(e.target.id){
+            case "Blue":
+                body.style.backgroundColor ='#00A5E3'
+                break;
+            case "Red":
+                body.style.backgroundColor ='#FF5768'
+                break;
+            case "DarkYellow":
+                body.style.backgroundColor = '#FFBF65'
+                break;
+            case "Orange":
+                body.style.backgroundColor = '#FC6238'
+                break;
+            default:
+                body.style.backgroundColor = 'black'
+                break;
+         }
+    })
+})
